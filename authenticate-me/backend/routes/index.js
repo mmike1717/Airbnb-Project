4ceb5333723f1de5c72d0bfc90c8dc546d600119
Michael <michael.oyola@outlook.com>
2023-06-07 17:43:51 -0400

    phase 1 complete

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -2,10 +2,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+const apiRouter = require('./api');
+
+
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
 });
+
+router.use('/api', apiRouter);
+
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+
 
 
 
