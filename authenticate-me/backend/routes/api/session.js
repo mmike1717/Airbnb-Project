0085133aa299fad06542f220e7dc5f712a5f2492
Michael <michael.oyola@outlook.com>
2023-06-08 17:22:45 -0400

    Add User login backend endpoint

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -47,6 +47,15 @@ router.post(
 
 
 
+  // Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
 
 
 
