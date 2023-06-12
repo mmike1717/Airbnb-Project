4f4baed3bf2d0dec135d811923ff292e415726a9
Michael <michael.oyola@outlook.com>
2023-06-12 14:21:18 -0400

    commit

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,8 +3,12 @@ const express = require('express');
 const { Op } = require('sequelize');
 
 const router = express.Router();
+const {Spot} = require('../../db/models')
 
 
+router.get('/', async (req, res) => {
+
+})
 
 
 
