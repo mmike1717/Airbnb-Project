e8aaa34a2988eb65887a62410413f3c19487fcfc
Michael <michael.oyola@outlook.com>
2023-06-12 16:19:59 -0400

    fixing errors

--- backend/db/migrations/20230611142845-create-spot.js
+++ backend/db/migrations/20230611142845-create-spot.js
@@ -1,8 +1,16 @@
 'use strict';
+
+
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Spots', {
+    return queryInterface.createTable("Spots", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -49,9 +57,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    options.tableName = 'Spots'
+    return queryInterface.dropTable(options);
   }
 };
